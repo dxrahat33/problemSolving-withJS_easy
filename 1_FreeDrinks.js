a44749e735343647d6a74fccfcbe070699216032
dxrahat33 <dxrahat33@gmail.com>
2024-08-30 14:56:38 +0600

    first problem solved

---
+++ 1_FreeDrinks.js
@@ -0,0 +1,15 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+let burger = 550;
+
+if(burger >=500){
+    console.log("Won a Free COKE")
+}
+else{
+    console.log("30TK per COKE")
+}
