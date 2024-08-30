f1c6930c862d0468c32e6b6d0f1e235d4fa67c5d
dxrahat33 <dxrahat33@gmail.com>
2024-08-30 20:52:08 +0600

    number 3 grade system done

--- 3_GradeCalculator.js
+++ 3_GradeCalculator.js
@@ -11,3 +11,20 @@ Create a simple JavaScript program that takes a student's score as input and ret
     F: 0-59
 
 ***/
+const score = prompt("Enter Your Result:");
+
+if(score >= 90 && score <= 100) {
+    console.log("You got the GPA: A")
+}
+else if (score >= 80 && score <= 89) {
+    console.log("You got the GPA: B")
+}
+else if(score >= 70 && score <= 79) {
+    console.log("You got the GPA: C")
+}
+else if (score >= 60 && score <= 69) {
+    console.log("You got the GPA: D")
+}
+else {
+    console.log("You got the GPA: F")
+}
