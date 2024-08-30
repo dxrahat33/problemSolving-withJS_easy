540f9552f34557931d52ee166e1ec7ef206febb1
dxrahat33 <dxrahat33@gmail.com>
2024-08-30 21:45:07 +0600

    ternary number done

--- 5_ternaryNumber.js
+++ 5_ternaryNumber.js
@@ -7,6 +7,18 @@ if num1 is bigger than num2 then result will be double of num1. if not, then the
 
 write a simple if-else. 
 
-also, write it using ternary operator.
+also, write it using .
 
  */
+
+const num1 = 2;
+const num2 = 3;
+let result;
+if(num1 > num2) {
+    result = num1 + num1;
+}
+else{
+    result = num1 + num2;
+}
+
+console.log(result)
