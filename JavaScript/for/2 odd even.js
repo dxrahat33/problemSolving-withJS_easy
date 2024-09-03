e5c0b2c92c6853a7aa24eba2379d7a89f7ac3273
dxrahat33 <dxrahat33@gmail.com>
2024-09-03 09:47:54 +0600

    for loop problem solved

--- JavaScript/for/2 odd even.js
+++ JavaScript/for/2 odd even.js
@@ -16,3 +16,17 @@ Find all the even numbers from 78 to 98.
  */
 
 /*programming hero*/
+
+// odd numbers 61-100
+for (let i = 61; i <= 100; i++) {
+    if (i % 2 !== 0) {
+        console.log(i)
+    }
+}
+console.log('new');
+// even number 78-98
+for (let n = 78; n <= 98; n++) {
+    if (n % 2 === 0) {
+        console.log(n)
+    }
+}
--- JavaScript/for/4 multiplication table.js
+++ JavaScript/for/4 multiplication table.js
@@ -6,3 +6,10 @@ Generate a multiplication table for number 9
 
 
 /*programming hero*/
+
+let number = 9;
+
+for (let i = 1; i <= 10; i++) {
+  console.log(number + " x " + i + " = " + (number * i));
+}
+
